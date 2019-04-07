90971abcd1c4db90ba96ef8abaf975e65f942b7d
Thymios <douvisthym@gmail.com>
2019-04-07 09:47:57 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `https://api.darksky.net/forecast/8f7c2addf8a3a15118cecad5adedd4d0/${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}?units=si`
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather servive', undefined)
+        } else if (!body.currently) {
+            callback("Unable to find data. Try another search.", undefined)
+        } else {
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+        }
+    })
+}
+
+module.exports = forecast
